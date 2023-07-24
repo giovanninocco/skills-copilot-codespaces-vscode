f9636ba5e2b24cf248597b8023c53738c9a89eaf
Giovanni Emanuele Nocco <giovanni.nocco@gmail.com>
2023-07-24 06:07:10 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var memberValue = member.options[member.selectedIndex].value;
+  var memberText = member.options[member.selectedIndex].text;
+  var memberSkills = document.getElementById("memberSkills");
+  var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+  }
+
+
